32876cf76613a29884b04f70e6fe076395d89de9
Thomas Skubicki <tskubicki@gmail.com>
2015-07-01 12:02:10 -0400

    added models.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require './models.rb'
 
 set :database,"sqlite3:microblog_db.sqlite3"
+
+
--- db/migrate/20150701154419_initial_schema_setup.rb
+++
@@ -1,4 +0,0 @@
-class InitialSchemaSetup < ActiveRecord::Migration
-  def change
-  end
-end
