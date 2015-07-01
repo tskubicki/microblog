985933d339f54e66df5b359666489301846d7040
Thomas Skubicki <tskubicki@gmail.com>
2015-07-01 13:11:11 -0400

    working bare login page

--- app.rb
+++ app.rb
@@ -4,4 +4,19 @@ require './models.rb'
 
 set :database,"sqlite3:microblog_db.sqlite3"
 
+get '/sign-up' do
+	erb :sign_up
+end
+
+post '/sign-up' do
+	confirmation = params[:confirm_password]
+	
+	if confirmation == params[:user][:password]
+		@user = User.create(params[:user])
+		"Signed up #{@user.username}"
+	else
+		"Not signed up. Check your passwords"
+	end
+
+end
 
